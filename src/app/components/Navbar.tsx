08f43fd4050592def41ede17af51280b0562052d
sananbsd2024 <sananbsd2024@gmail.com>
2024-12-07 15:22:05 +0700

    Add files via upload

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,17 +1,75 @@
-import Link from "next/link"
+'use client'
 
-export default function Navbar() {
-    return (
-        <nav className="bg-blue-800 p-4">
-            <ul className="flex justify-evenly text-2xl font-bold">
-                <li><Link href="/">Home</Link></li>
-                <li><Link href="/api/auth/signin">Sign In</Link></li>
-                <li><Link href="/api/auth/signout">Sign Out</Link></li>
-                <li><Link href="/server">Server</Link></li>
-                <li><Link href="/client">Client</Link></li>
-                <li><Link href="/extra">Extra</Link></li>
-            </ul>
-        </nav>
-    )
-}
+import React, { useState } from 'react';
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
+import Link from 'next/link'; // Import the Link component from Next.js
 
+const Navbar = () => {
+  // State to manage the navbar's visibility
+  const [nav, setNav] = useState(false);
+
+  // Toggle function to handle the navbar's display
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
+  // Array containing navigation items with links
+  const navItems = [
+    { id: 1, text: 'Home', link: '/' },
+    { id: 2, text: 'Company', link: '/company' },
+    { id: 3, text: 'Resources', link: '/resources' },
+    { id: 4, text: 'About', link: '/about' },
+    { id: 5, text: 'Contact', link: '/contact' },
+    { id: 6, text: 'Logout', link: '/api/auth/signout' },
+  ];
+
+  return (
+    <div className='bg-gray-100 flex justify-between 
+    items-center h-24 max-w-[1240px] mx-auto px-4 text-blue-800'>
+      {/* Logo */}
+      <h1 className='w-full text-3xl font-bold text-[#00df9a]'>Nong Berd</h1>
+
+      {/* Desktop Navigation */}
+      <ul className='hidden md:flex'>
+        {navItems.map(item => (
+          <li
+            key={item.id}
+            className='p-3 hover:bg-[#00df9a] rounded-xl m-2 
+            cursor-pointer duration-300 hover:text-black'
+          >
+            <Link href={item.link}>{item.text}</Link>
+          </li>
+        ))}
+      </ul>
+
+      {/* Mobile Navigation Icon */}
+      <div onClick={handleNav} className='block md:hidden'>
+        {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+      </div>
+
+      {/* Mobile Navigation Menu */}
+      <ul
+        className={
+          nav
+            ? 'fixed md:hidden left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500'
+            : 'ease-in-out w-[60%] duration-500 fixed top-0 bottom-0 left-[-100%]'
+        }
+      >
+        {/* Mobile Logo */}
+        <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>nongberd</h1>
+
+        {/* Mobile Navigation Items */}
+        {navItems.map(item => (
+          <li
+            key={item.id}
+            className='p-4 border-b rounded-xl hover:bg-[#00df9a] duration-300 hover:text-black cursor-pointer border-gray-600'
+          >
+            <Link href={item.link}>{item.text}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
