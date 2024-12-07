e1b21ea083c2836470ddb7e14c514838ad5897c2
sananbsd2024 <sananbsd2024@gmail.com>
2024-12-07 22:31:16 +0700

    Create Gallery.tsx

---
+++ src/app/components/Gallery.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const GalleryPage = () => {
+  return (
+    <div>
+    <h1 className="rounded text-xl font-bold mb-6 bg- p-2 mx-auto 
+      flex justify-center text-white">ภาพกิจกรรม</h1>
+    </div>
+  )
+}
+
+export default GalleryPage
