d06f6db722d426dcae644fab9540b6d4c2271ced
sananbsd2024 <sananbsd2024@gmail.com>
2024-12-07 20:14:21 +0700

    Update RightDashboard.tsx

--- src/app/dashboard/RightDashboard.tsx
+++ src/app/dashboard/RightDashboard.tsx
@@ -40,12 +40,11 @@ const RightDashboardPage = () => {
       <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         <a href="/dashboard/add-newslist">
           <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            เพิ่มข้อข่าว
+            ข่าวประชาสัมพันธ์
           </h5>
         </a>
         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-          Here are the biggest enterprise technology acquisitions of 2021 so
-          far, in reverse chronological order.
+          เพิ่มข่าวประชาสัมพันธ์
         </p>
         <a
           href="/dashboard/add-newslist"
@@ -70,6 +69,38 @@ const RightDashboardPage = () => {
         </a>
       </div>
 
+            <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <a href="/dashboard/add-newssell">
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            ข่าวประกวดราคา
+          </h5>
+        </a>
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+          เพิ่มข่าวประกวดราคา/จัดซื้อจัดจ้าง
+        </p>
+        <a
+          href="/dashboard/add-newssell"
+          className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          เพิ่มข้อข่าว
+          <svg
+            className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 14 10"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M1 5h12m0 0L9 1m4 4L9 9"
+            />
+          </svg>
+        </a>
+      </div>
+      
     </div>
   );
 };
