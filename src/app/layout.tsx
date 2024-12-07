b55508e66733d91f5512b4b763c83d4b23ccf337
sananbsd2024 <sananbsd2024@gmail.com>
2024-12-07 15:38:33 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,13 +2,15 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import Navbar from './components/Navbar'
 import AuthProvider from './context/AuthProvider'
+import FooterPage from "./components/Footer";
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata = {
-  title: 'NextAuth Tutorial',
-  description: 'Learn NextAuth.js by Dave Gray',
-}
+  title: "โรงเรียนบ้านหนองเบิด",
+  description:
+    "โรงเรียนบ้านหนองเบิด ตำบลเมืองน้อย อำเภอธวัชบุรี จังหวัดร้อยเอ็ด",
+  keywords: "Nongberd School, โรงเรียนบ้านหนองเบิด, หนองเบิด",
+};
 
 export default function RootLayout({
   children,
@@ -23,6 +25,7 @@ export default function RootLayout({
           <main className="flex justify-center items-start p-6 min-h-screen">
             {children}
           </main>
+        <FooterPage />
         </AuthProvider>
       </body>
     </html>
