6ea49bd77031e6049e97060cf3cbb3ff371aa304
sananbsd2024 <sananbsd2024@gmail.com>
2024-12-07 21:06:00 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
-import { options } from "./api/auth/[...nextauth]/options"
-import { getServerSession } from "next-auth/next"
+import { options } from "./api/auth/[...nextauth]/options";
+import { getServerSession } from "next-auth/next";
+// import UserCard from "./components/UserCard"
 import LeftSidebarPage from "./components/LeftSidebar";
-import RightSidebarPage from "./components/RightSidebar";
 import RightContentAreaPage from "./components/RightContentArea";
+import RightSidebarPage from "./components/RightSidebar";
 
 export default async function Home() {
-  const session = await getServerSession(options)
+  const session = await getServerSession(options);
 
   return (
     <>
@@ -15,34 +16,33 @@ export default async function Home() {
         <h1 className="text-5xl">You Shall Not Pass!</h1>
       )} */}
 
-      <div className="w-full h-full min-h-screen flex flex-col md:flex-row">
+      <div className="min-h-screen flex flex-col md:flex-row required:xl">
         {/* ส่วนซ้าย */}
-        <div className="w-full md:w-1/4 bg-gray-200 text-white p-4">
+        <div className="w-full md:w-1/4 p-4">
           {/* <h2 className="text-xl font-bold">Left Sidebar</h2> */}
-          <p>This is the left sidebar.</p>
-<LeftSidebarPage />
+          {/* <p>This is the left sidebar.</p> */}
 
-
+          <LeftSidebarPage />
         </div>
 
         {/* ส่วนขวา */}
-        <div className="w-full md:w-3/4 bg-gray-100 p-4">
-          <h2 className="text-xl font-bold">Right Content Area</h2>
-
-<RightContentAreaPage />
+        <div className="w-full md:w-3/4 p-4">
+          {/* <h2 className="text-xl font-bold">Right Content Area</h2> */}
+          <RightContentAreaPage />
 
           {/* <p>This is the main content area on the right.</p> */}
         </div>
 
-        <div className="w-full md:w-1/4 bg-gray-200 text-white p-4">
+        <div className="w-full md:w-1/4 p-4">
           {/* <h2 className="text-xl font-bold">Left Sidebar</h2> */}
-          <p>This is the left sidebar.</p>
-<RightSidebarPage />
-          
+          {/* <p>This is the left sidebar.</p> */}
+
+          <RightSidebarPage />
         </div>
       </div>
 
 
     </>
-  )
+    
+  );
 }
