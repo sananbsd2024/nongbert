fc80f2eddf42168e695827005712d41905435aef
sananbsd2024 <sananbsd2024@gmail.com>
2024-12-07 16:03:16 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <AuthProvider>
           <Navbar />
-          <main className="flex justify-center items-start p-6 min-h-screen">
+          <main className="flex justify-center items-start p-2 min-h-screen">
             {children}
           </main>
         <FooterPage />
