725d972e9b4849cbcc011f8f4f4a7cfd5c1a6075
sananbsd2024 <sananbsd2024@gmail.com>
2024-12-07 19:45:04 +0700

    Create page.tsx

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import RightDashboardPage from './RightDashboard'
+
+const DashboardPage = () => {
+  return (
+    <div>
+      <h1>Dashboard Page</h1>
+      <RightDashboardPage />
+    </div>
+  )
+}
+
+export default DashboardPage
