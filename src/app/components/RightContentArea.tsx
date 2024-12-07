1fc3ad276da7bc5768ca99fc32cc685b807677a0
sananbsd2024 <sananbsd2024@gmail.com>
2024-12-07 22:31:57 +0700

    Update RightContentArea.tsx

--- src/app/components/RightContentArea.tsx
+++ src/app/components/RightContentArea.tsx
@@ -1,12 +1,13 @@
 import NewsListPage from "./Newslist";
 import NewsSellListPage from "./Newssell";
-
+import GalleryPage from "./Gallery";
 
 export default function RightContentAreaPage() {
     return (
         <div>
             <NewsListPage />
             <NewsSellListPage />
+            <GalleryPage />
         </div>
     );
 }
