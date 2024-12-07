5fdaff659c1cc00c6761def4f7323bae9989830b
sananbsd2024 <sananbsd2024@gmail.com>
2024-12-07 22:32:40 +0700

    Update Gallery.tsx

--- src/app/components/Gallery.tsx
+++ src/app/components/Gallery.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const GalleryPage = () => {
   return (
     <div>
-    <h1 className="rounded text-xl font-bold mb-6 bg- p-2 mx-auto 
+    <h1 className="rounded text-xl font-bold mb-6 bg-gray-500 p-2 mx-auto 
       flex justify-center text-white">ภาพกิจกรรม</h1>
     </div>
   )
