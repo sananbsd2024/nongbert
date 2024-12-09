a3b088d615ff88146141c41cb6b31798f1a5e8d6
sananbsd2024 <sananbsd2024@gmail.com>
2024-12-09 10:37:07 +0700

    Create Student.ts

---
+++ src/app/models/Student.ts
@@ -0,0 +1,23 @@
+import mongoose, { Schema, model, models, Document } from 'mongoose';
+
+interface IStudent extends Document {
+  fristname: string;
+  lastname: string;
+  g_level: string;
+  grade: string;
+  age: string;
+  createdAt: Date;
+}
+
+const StudentSchema = new Schema<IStudent>({
+  fristname: { type: String, required: true },
+  lastname: { type: String, required: true },
+  g_level: { type: String },
+  grade: { type: String },
+  age: { type: String },
+  createdAt: { type: Date, default: Date.now },
+});
+
+const Student = models.Student || model<IStudent>('Student', StudentSchema);
+
+export default Student;
