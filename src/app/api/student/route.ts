f77dc6f61b715ef935809f33f5f63462a38dfca4
sananbsd2024 <sananbsd2024@gmail.com>
2024-12-09 21:58:00 +0700

    Update route.ts

--- src/app/api/student/route.ts
+++ src/app/api/student/route.ts
@@ -15,10 +15,10 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       }
 
       // Create a new user
-      const user = new User({ firstname, lastname, role, grade, age });
-      await user.save();
+      const student = new Students({ firstname, lastname, role, grade, age });
+      await student.save();
 
-      res.status(201).json({ success: true, message: 'User added successfully!', user });
+      res.status(201).json({ success: true, message: 'Students added successfully!', user });
     } catch (error) {
       res.status(500).json({ success: false, message: 'Internal server error', error });
     }
