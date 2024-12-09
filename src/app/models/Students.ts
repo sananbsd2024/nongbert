e1e189eec45fefde805a15b91ac22458d3f3a27b
sananbsd2024 <sananbsd2024@gmail.com>
2024-12-09 21:55:24 +0700

    Update Students.ts

--- src/app/models/Students.ts
+++ src/app/models/Students.ts
@@ -1,29 +1,13 @@
 import mongoose, { Schema, model, models } from 'mongoose';
 
-const StudensSchema = new Schema({
-
-  fristname: {
-    type: String,
-//    required: [false, 'Fristname is required'], // ชื่อเป็นข้อมูลที่จำเป็น
-  },
-  lastname: {
-    type: String,
-//    required: [false, 'Lastname is required'], // ตำแหน่งเป็นข้อมูลที่จำเป็น
-  },
-  grade: {
-    type: String,
-//    required: [false, 'Grade is required'], // ตำแหน่งเป็นข้อมูลที่จำเป็น
-  },
-  age: {
-    type: String,
-//    required: [false, 'Age is required'], // ตำแหน่งเป็นข้อมูลที่จำเป็น
-  },
-  role: {
-    type: String,
-    enum: ['director', 'teacher', 'staff'], // จำกัดค่า role เฉพาะ 3 ค่า
-    required: [false, 'Role is required'], // role เป็นข้อมูลที่จำเป็น
-  },
+const StudentsSchema = new Schema({
+  firstname: { type: String, required: true },
+  lastname: { type: String, required: true },
+  role: { type: String, required: true },
+  grade: { type: String, required: true },
+  age: { type: Number, required: true },
 }, { timestamps: true });
 
-const Studens = models.Studens || model('Studens', StudensSchema);
-export default Studens;
+const Students = models.Students || model('Students', StudentsSchema);
+
+export default Students;
