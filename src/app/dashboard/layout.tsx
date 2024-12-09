2697a5230106681f895796c2bb1730df10b6798b
sananbsd2024 <sananbsd2024@gmail.com>
2024-12-09 20:07:35 +0700

    Update layout.tsx

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,3 +1,5 @@
+import MenuListPage from "../components/Menulists";
+
 export default function DashboardLayout({
   children,
 }: {
@@ -10,6 +12,32 @@ export default function DashboardLayout({
         {/* <h2 className="text-xl font-bold">Left Sidebar</h2> */}
         <p>This is the left sidebar.</p>
         {/* <LeftSidebarPage /> */}
+        <MenuListPage />
+        <br />
+
+
+        <a
+          href="/api/auth/signout"
+          className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          ออกจากระบบ
+          <svg
+            className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 14 10"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M1 5h12m0 0L9 1m4 4L9 9"
+            />
+          </svg>
+        </a>
+
       </div>
 
       {/* ส่วนขวา */}
