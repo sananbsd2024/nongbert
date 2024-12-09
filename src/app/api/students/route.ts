5a1d9e2a3b5f2fe2553b5cb05cc2d14f9dfca06f
sananbsd2024 <sananbsd2024@gmail.com>
2024-12-09 21:31:36 +0700

    Update route.ts

--- src/app/api/students/route.ts
+++ src/app/api/students/route.ts
@@ -1,12 +1,12 @@
 import { NextResponse } from 'next/server';
 import { connectToDatabase } from '@/app/lib/mongoose';
-import Student from '@/app/models/Student';
+import Students from '@/app/models/Students';
 
 export async function POST(request: Request) {
   try {
     await connectToDatabase();
     const body = await request.json();
-    const newStudent = new Student(body);
+    const newStudent = new Students(body);
     const savedStudent = await newStudent.save();
     return NextResponse.json({ success: true, data: savedStudent }, { status: 201 });
   } catch (error) {
@@ -18,7 +18,7 @@ export async function POST(request: Request) {
 export async function GET() {
   try {
     await connectToDatabase();
-    const students = await Student.find().sort({ createdAt: -1 });
+    const students = await Students.find().sort({ createdAt: -1 });
     return NextResponse.json({ success: true, data: students }, { status: 200 });
   } catch (error) {
     console.error('Error fetching Students:', error);
