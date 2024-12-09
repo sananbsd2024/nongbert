eed7ecd57a591cd2f81b6161983119e6d953e4c2
sananbsd2024 <sananbsd2024@gmail.com>
2024-12-09 22:19:01 +0700

    Update Student.ts

--- src/app/models/Student.ts
+++ src/app/models/Student.ts
@@ -3,7 +3,7 @@ import mongoose, { Schema, model, models, Document } from 'mongoose';
 interface IStudent extends Document {
   fristname: string;
   lastname: string;
-  role: string;
+  glevel: string;
   grade: string;
   age: string;
   createdAt: Date;
@@ -12,7 +12,7 @@ interface IStudent extends Document {
 const StudentSchema = new Schema<IStudent>({
   fristname: { type: String, required: true },
   lastname: { type: String, required: true },
-  role: { type: String },
+  glevel: { type: String },
   grade: { type: String },
   age: { type: String },
   createdAt: { type: Date, default: Date.now },
