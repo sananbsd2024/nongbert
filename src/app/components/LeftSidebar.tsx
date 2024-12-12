c12b1f4761f14b7c294949e15bc5bb14e109b7d7
sananbsd2024 <sananbsd2024@gmail.com>
2024-12-12 22:55:19 +0700

    Update LeftSidebar.tsx

--- src/app/components/LeftSidebar.tsx
+++ src/app/components/LeftSidebar.tsx
@@ -1,37 +1,22 @@
-// import PageViews from './PageView';
-// import BasicCard from './BasicCard';
-import MenuListPage from './Menulists';
-import MenuList2Page from './Menulist2';
-// import FacebookCardTailwind from './FacebookCardTailwind';
-// import CommanderPage from './Commander';
-// import LoginPage from './LoginForm';
-// import DirectorProfile from './DirectorProfile';
-
+import MenuListPage from "./Menulists";
+import MenuList2Page from "./Menulists2";
+import MenuList3Page from "./Menulists3";
 
 export default function LeftSidebarPage() {
-    return (
-        <div>
-            {/* <DirectorProfile /> */}
-             {/* <CommanderPage /> */}
-            <br />
-            {/* <PageViews /> */}
-            {/* <BasicCard /> */}
-            <br />
-            <div className="border rounded-lg shadow-sm mb-4">
-                <MenuListPage />
-            </div>
-            <div className="border rounded-lg shadow-sm mb-4">
-                <MenuList2Page />
-            </div>
+  return (
+    <div>
+      <br />
+      <div className="border rounded-lg shadow-sm mb-4">
+        <MenuListPage />
+      </div>
 
-            {/* <div className="container mt-5">
-                <h1 className="text-center mb-4">Facebook Post Card Example</h1>
-                <FacebookCard />
-            </div> */}
+      <div className="border rounded-lg shadow-sm mb-4">
+        <MenuList2Page />
+      </div>
 
-            {/* <FacebookCardTailwind /> */}
-
-            {/* <LoginPage /> */}
-        </div>
-    );
+      <div className="border rounded-lg shadow-sm mb-4">
+        <MenuList3Page />
+      </div>
+    </div>
+  );
 }
