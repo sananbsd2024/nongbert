f19b19e657b4f5d43807ef66c5cf7411c57816bc
sananbsd2024 <sananbsd2024@gmail.com>
2024-12-12 22:00:49 +0700

    Update Menulists.tsx

--- src/app/components/Menulists.tsx
+++ src/app/components/Menulists.tsx
@@ -4,26 +4,32 @@ export default function MenuListPage() {
   return (
     <div className="p-4">
       <div className="divide-y divide-gray-400">
-        <a href="" className="font-bold text-blue-500">
+        <div className="bg-blue-600 p-2 rounded-sm">
+        <a href="" className="font-bold text-white">
           ข้อมูลพื้นฐาน
         </a>
+        </div>
+
         <div className="flex flex-col grid grid-cols-1 divide-y gap-2 text-blue-500">
-          <Link href="/info" className="mx-2">
+          <Link href="/info" className="mx-2 p-1">
             ข้อมูลพื้นฐานโรงเรียน
           </Link>
-          <Link href="/history" className="mx-2">
+          <Link href="/history" className="mx-2 p-1">
             ประวัติหน่วยงาน
           </Link>
-          <Link href="/vision" className="mx-2">
+          <Link href="/vision" className="mx-2 p-1">
             วิสัยทัศน์ / ปรัชญา
           </Link>
-          <Link href="/kk_sch" className="mx-2">
+          <Link href="/kk_sch" className="mx-2 p-1">
             คณะกรรมการสถานศึกษา
           </Link>
-          <Link href="/employeelist" className="mx-2">
+          {/* <Link href="/employeelist" className="mx-2">
             บุคลากรทางการศึกษา
+          </Link> */}
+          <Link href="/eservice" className="mx-2 p-1">
+            e-service
           </Link>
-          <Link href="/dashboard" className="mx-2">
+          <Link href="/dashboard" className="mx-2 p-1">
             ผู้ดูแลระบบ
           </Link>
         </div>
