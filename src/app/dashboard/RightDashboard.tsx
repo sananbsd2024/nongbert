40747f290cb76b2fd23674e425af904e7b92a89b
sananbsd2024 <sananbsd2024@gmail.com>
2024-12-26 06:52:03 +0700

    Update RightDashboard.tsx

--- src/app/dashboard/RightDashboard.tsx
+++ src/app/dashboard/RightDashboard.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const RightDashboardPage = () => {
   return (
-    <div className="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 place-items-center gap-6">
+    <div className="w-[250px] grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 place-items-center gap-6">
       <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         <a href="/dashboard/add-employee">
           <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
